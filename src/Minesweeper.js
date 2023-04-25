c248120613106a0e52dfbb5bcb7239d03a3409bc
nicolynicole <107006884+nicolynicole@users.noreply.github.com>
2023-04-25 17:56:39 -0400

    added 🚩 emoji, changed CSS styling, added bfs function

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -3,6 +3,14 @@ import './Minesweeper.css'
 
 const MAX_BOMBS = 30;
 const SIZE_OF_BOARD = 10;
+const directions = [{r_offset : -1, c_offset : -1},  // top left
+                    {r_offset : -1, c_offset :  0},  // top
+                    {r_offset : -1, c_offset :  1},  // top right
+                    {r_offset :  0, c_offset : -1},  // left
+                    {r_offset :  0, c_offset :  1},  // right
+                    {r_offset :  1, c_offset : -1},  // bottom left
+                    {r_offset :  1, c_offset :  0},  // bottom
+                    {r_offset :  1, c_offset :  1}]; // bottom right
 
 function generateBombLocations(){
     let bombLocations = Array.from(Array(SIZE_OF_BOARD), () => new Array(SIZE_OF_BOARD).fill(0));
@@ -31,15 +39,6 @@ function inBounds(row, col){
 function generateNumBombNeighbors(bombLocations){
 
     let board = Array.from(Array(SIZE_OF_BOARD), () => new Array(SIZE_OF_BOARD).fill(0));;
-        
-        const directions = [{r_offset : -1, c_offset : -1}, 
-                            {r_offset : -1, c_offset :  0}, 
-                            {r_offset : -1, c_offset :  1}, 
-                            {r_offset :  0, c_offset : -1}, 
-                            {r_offset :  0, c_offset :  1},
-                            {r_offset :  1, c_offset : -1}, 
-                            {r_offset :  1, c_offset :  0}, 
-                            {r_offset :  1, c_offset :  1}]
         
         for (let row = 0; row < SIZE_OF_BOARD; row++){
             for (let col = 0; col < SIZE_OF_BOARD; col++){
@@ -100,32 +99,9 @@ export default function Minesweeper () {
     }
 
 
-
-    function onSquarePressed(r, c) {
-
-        if (board[r][c] === "F") return;
-
-         if (bombLocations[r][c] === 1){
-            let b = copyBoard();
-            b[r][c] = "💣";
-            setBoard(b);
-            setIsGameOver(true);
-            return;
-        }
-
-        if (numBombNeighbors[r][c] !== 0){
-            let b = copyBoard();
-            b[r][c] = numBombNeighbors[r][c];
-            setBoard(b);
-            return;
-        }
-
-        // else no flag, no bomb, and no number
-        // run a BFS to find perimeter 
+    function findSquaresToReveal(r,c){
+        
         let queue = [];
-        // explore each direction until we reach a position on the board
-        // occupied by a number
-
         queue.push({r, c});
         let visited = [];
         let squaresToReveal = [];
@@ -137,8 +113,7 @@ export default function Minesweeper () {
             }
         }
         visited[r][c] = true;
-        //squaresToReveal.push({r, c});
-
+    
         while (queue.length > 0){
            const indices = queue[0]; // take out oldest 
            queue.shift();
@@ -146,21 +121,12 @@ export default function Minesweeper () {
 
            if (numBombNeighbors[indices.r][indices.c] !== 0) continue; // stop here
             // else keep exploring 
-            const directions = [{r_offset : -1, c_offset : -1}, 
-                {r_offset : -1, c_offset :  0}, 
-                {r_offset : -1, c_offset :  1}, 
-                {r_offset :  0, c_offset : -1}, 
-                {r_offset :  0, c_offset :  1},
-                {r_offset :  1, c_offset : -1}, 
-                {r_offset :  1, c_offset :  0}, 
-                {r_offset :  1, c_offset :  1}]
                 
             directions.forEach(direction => {
                 let n_row = indices.r + direction.r_offset;
                 let n_col = indices.c + direction.c_offset;
                 if (inBounds(n_row, n_col) && visited[n_row][n_col] === false){
                     if (bombLocations[n_row][n_col] !== 1){ // dont want to reveal bombs
-                        //let foundNonZero = numBombNeighbors[n_row][n_col] !== 0;
                         queue.push({r : n_row, c : n_col});
                         visited[n_row][n_col] = true;
                         squaresToReveal.push({row : n_row, col : n_col});
@@ -169,22 +135,50 @@ export default function Minesweeper () {
             });
         }
 
+        return squaresToReveal;
+    }
+
+    function onSquarePressed(r, c) {
+
+        if (board[r][c] === "🚩") return;
         let b = copyBoard();
+         
+        if (bombLocations[r][c] === 1){
+            b[r][c] = "💣";
+            setBoard(b);
+            setIsGameOver(true);
+            return;
+        }
+
+        if (numBombNeighbors[r][c] !== 0){
+            b[r][c] = numBombNeighbors[r][c];
+            setBoard(b);
+            return;
+        }
+
+        // else no flag, no bomb, and no number
+        // run a BFS to find perimeter 
+        // explore each direction until we reach a position on the board
+        // occupied by a number that isn't 0 and not a bomb
+
+        let squaresToReveal = findSquaresToReveal(r, c);
         squaresToReveal.forEach(squareToReveal => {
+            
             b[squareToReveal.row][squareToReveal.col] = numBombNeighbors[squareToReveal.row][squareToReveal.col];
 
         });
+
         setBoard(b);
     }
 
     function toggleFlag(event, r, c){
         event.preventDefault();
         
-        if (board[r][c] !== "" && board[r][c] !== "F") return;
+        if (board[r][c] !== "" && board[r][c] !== "🚩") return;
 
         let b = copyBoard();
 
-        b[r][c] = board[r][c] === "F" ? "" : "F";
+        b[r][c] = board[r][c] === "🚩" ? "" : "🚩";
         setBoard(b);
         
     }
@@ -196,7 +190,7 @@ export default function Minesweeper () {
             let buttons = [];
         
             for (let c = 0; c < SIZE_OF_BOARD; c++) {
-               buttons.push(<button key={`${r} + ${c}`} onContextMenu={(event) => toggleFlag(event, r, c)} onClick={() => onSquarePressed(r, c)} disabled={isGameOver ? true : false} className="matrix-button">{board[r][c]}</button>);
+               buttons.push(<button key={`${r} + ${c}`} onContextMenu={(event) => toggleFlag(event, r, c)} onClick={() => onSquarePressed(r, c)} disabled={isGameOver ? true : false} className={isGameOver ? "matrix-disabled-button" : board[r][c] !== "" && board[r][c] !== "🚩" ? "matrix-button-filled" : "matrix-button"}>{board[r][c] !== 0 ? board[r][c] : ""}</button>);
             }
         
             rows.push(<div key={r} id={`${r}`}className="matrix-row">{buttons}</div>);
